20c487005e0f0bc4a4a321089c5285ed486cc69d
Edgar <edgara.mendozaf@gmail.com>
2026-01-27 14:59:25 -0600

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/100-citas-together/",
   server: {
     host: "::",
     port: 8080,
