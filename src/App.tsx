f0b7525e1dc2851e3ad8c6e37181d96e3a64262b
Edgar <edgara.mendozaf@gmail.com>
2026-01-27 16:46:56 -0600

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/100-citas-together/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
